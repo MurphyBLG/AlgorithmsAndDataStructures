e1f4e561dae5c45420aa90bb1f515e97cf964c4e
MurphyBLG <erwin16@ya.ru>
2022-04-11 16:01:47 +0500

    Deleting implemented

--- Searches/2.py
+++ Searches/2.py
@@ -7,9 +7,9 @@ class Node:
 
 
 class BST:
-
     def __init__(self) -> None:
         self.root = None
+
 
     def build_tree(self, arr: list):
         if len(arr) == 0:
@@ -24,6 +24,7 @@ class BST:
         node.left = self.build_tree(arr[:mid])
         node.right = self.build_tree(arr[mid + 1:])
         return node
+
 
     def find(self, key: int):
         cur = self.root
@@ -41,6 +42,47 @@ class BST:
             return None
 
 
+    def find_min(self, node: Node):
+        cur = node
+        while cur.left != None:
+            cur = cur.left
+
+        return cur
+
+
+    def delete(self, node: Node, value: int):
+        if node is None:
+            return node
+    
+        if value < node.value:
+            node.left = self.delete(node.left, value)
+        elif value > node.value:
+            node.right = self.delete(node.right, value)
+        else:
+            if node.left is None:
+                temp = node.right
+                node = None
+                return temp
+            elif node.right is None:
+                temp = node.left
+                node = None
+                return temp
+    
+            temp = self.find_min(node.right)
+            node.value = temp.value
+            node.right = self.delete(node.right, temp.value)
+    
+        return node
+
+
+    def printTree(self, node, level=0):
+        if node != None:
+            self.printTree(node.left, level + 1)
+            print(' ' * 4 * level + '-> ' + str(node.value))
+            self.printTree(node.right, level + 1)
+
+
+
 if __name__ == '__main__':
     n = int(input())
     arr = list(map(int, input().split()))
@@ -50,4 +92,7 @@ if __name__ == '__main__':
     tree.build_tree(arr)
 
     print(tree.find(5))
+    tree.printTree(node=tree.root)
+    tree.delete(node=tree.root, value=6)
+    tree.printTree(node=tree.root)
 
